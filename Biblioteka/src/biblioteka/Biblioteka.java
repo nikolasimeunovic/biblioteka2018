19cfe5be162f8052392017cd7376cc5ca23f1ffa
nikolasimeunovic <sime97kv@gmail.com>
2018-03-09 11:07:35 +0100

    Revert "Dodati komentari"
    This reverts commit f618ba62d1bbfb438952ff7a6f3744557c574357.

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -29,7 +29,6 @@ public class Biblioteka implements Bibliotekainterfejs{
 
 	@Override
 	public LinkedList<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
-		//Ovo je ok komentar
 		if(naslov==null)
 			throw new RuntimeException("morate uneti naslov");
 		
